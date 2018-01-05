dabccb9f6272d035b73c4f0d3198c3c658eaed56
jingyu16 <jingyu16@staff>
2018-01-05 22:12:54 +0800

    修改示例代码

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -10,14 +10,14 @@ const rep = new Report({
   mergeReport: true, // mergeReport 是否合并上报， false 关闭， true 启动（默认）
   delay: 1000, // 当 mergeReport 为 true 可用，延迟多少毫秒，合并缓冲区中的上报（默认）
   url: 'http://localhost:80', // 指定错误上报地址
+  getPath: '/read.gif', // get请求路径
+  postPath: '/post/jserr', // post请求路径
   random: 1, // 抽样上报，1~0 之间数值，1为100%上报（默认 1）
 });
 
 rep.on('afterReport', function () {
   console.log('afterReport');
 });
-rep.reportByPost('/post/jserr', {
-  msg: '1111'
-}, function (data) {
+rep.reportByPost({msg: '1111'}, function (data) {
   console.log(data);
 });
